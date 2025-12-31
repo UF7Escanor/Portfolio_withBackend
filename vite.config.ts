5695845783bf3b89890a46ddbcadc7aa409f366f
rahul <nameisrahulsenthakuri@gmail.com>
2025-12-31 19:01:31 +0545

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,24 +9,25 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      //for netify
+      base: "/",
     },
   },
   server: {
     proxy: {
       // Proxy /api requests to backend running on localhost:3000
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
       },
-      '/uploads': {
-        target: 'http://localhost:3000',
+      "/uploads": {
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
       },
     },
   },
-})
-
+});
